d9c5c82c2091779741b4b5f79735c2e2c169cc09
Adrians Heidens <adrians.heidens@gmail.com>
2018-07-22 12:23:31 +0300

    HttpClient usage examples.

---
+++ HttpClientLearn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HttpClientLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making HTTP request...");
+
+            HttpClient httpClient = new HttpClient();
+
+            var response = httpClient.GetAsync("https://google.lv").Result;
+            Console.WriteLine(response);
+
+            Console.WriteLine("End.");
+            Console.ReadKey();
+        }
+    }
+}
