79d65b2c458f632d1492da52d457d526a1fe8bd8
Adrians Heidens <adrians.heidens@gmail.com>
2018-07-24 12:33:02 +0300

    Log4Net example.

---
+++ Log4NetLearn/Program.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Reflection;
+
+namespace Log4NetLearn
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure(LogManager.GetRepository(Assembly.GetCallingAssembly()));
+
+            log.Debug("Debug message from logger");
+            log.Debug("Error message from logger");
+
+            Console.WriteLine("End.");
+            Console.ReadKey();
+        }
+    }
+}
