cf69f4d29dde8b214a2f08abb2ce9be632dd9790
Adrians Heidens <adrians.heidens@gmail.com>
2018-08-21 23:55:09 +0300

    Learn streams.

---
+++ StreamLearn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace StreamLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            memoryStream.Write(BitConverter.GetBytes((ushort) 65535));
+            memoryStream.Write(BitConverter.GetBytes((int) 120));
+
+            Console.WriteLine(BitConverter.ToString(memoryStream.ToArray()));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StreamLearn/README.txt
@@ -0,0 +1,15 @@
+﻿Demonstrates stream usage.
+
+A stream is an abstraction of a sequence of bytes, such as a file, an input/
+output device, an inter-process communication pipe, or a TCP/IP socket.
+
+* You can read from streams.
+* You can write to streams.
+* Streams can support seeking.
+
+* Stream (abstract)
+* FileStream
+* MemoryStream
+* BufferedStream
+
+* Implements IDisposable.
