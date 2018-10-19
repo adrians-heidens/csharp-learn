41560779ffff3f6928eed7180f8c878f01e45a70
Adrians Heidens <adrians.heidens@gmail.com>
2018-10-19 10:22:50 +0300

    Http request example for Json.

---
+++ HttpClientLearn/JsonRequestExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpClientLearn
+{
+    static class JsonRequestExample
+    {
+        public static void Run()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://127.0.0.1:8080");
+            httpClient.Timeout = TimeSpan.FromSeconds(5);
+            string jsonString = "{foo: 1, bar: 'spam'}";
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            try
+            {
+                response = httpClient.PostAsync("/foo", content).Result;
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+                return;
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var body = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Error: status={(int)response.StatusCode}, body='{body}'");
+            }
+            else
+            {
+                var body = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Success: status={(int)response.StatusCode}, body='{body}'");
+            }
+        }
+    }
+}
