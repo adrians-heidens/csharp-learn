a1df6c35d6b40be9bb904785cb772648f5232b24
Adrians Heidens <adrians.heidens@gmail.com>
2018-10-20 13:34:13 +0300

    Number parse example.

---
+++ NumberParseLearn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumberParseLearn
+{
+    /// <summary>
+    /// Demonstrates number parsing.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ushortHex = "FEB1";
+            var num = ushort.Parse(ushortHex, NumberStyles.HexNumber);
+            Console.WriteLine(num);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
