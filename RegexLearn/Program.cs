07d25a073212447b798dbef2726582d498e3b780
Adrians Heidens <adrians.heidens@gmail.com>
2018-10-22 22:59:42 +0300

    Regex example.

---
+++ RegexLearn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex("^[a-z]([a-z0-9-]*[a-z0-9])?$", RegexOptions.IgnoreCase);
+            Console.WriteLine(regex.IsMatch("abe"));
+            Console.WriteLine(regex.IsMatch("abe-"));
+            Console.WriteLine(regex.IsMatch("abe0"));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
