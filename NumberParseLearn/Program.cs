44c6dace8a65d34b2f619cd0f25b4dad32b2942b
Adrians Heidens <adrians.heidens@gmail.com>
2018-10-23 23:09:48 +0300

    More number parse examples.

--- NumberParseLearn/Program.cs
+++ NumberParseLearn/Program.cs
@@ -13,6 +13,17 @@ namespace NumberParseLearn
             var ushortHex = "FEB1";
             var num = ushort.Parse(ushortHex, NumberStyles.HexNumber);
             Console.WriteLine(num);
+
+            // From hex string to number.
+            ushortHex = "00Ba";
+            ushort val = 0;
+            var result = ushort.TryParse(ushortHex, NumberStyles.HexNumber, null, out val);
+            Console.WriteLine(result);
+            Console.WriteLine(val);
+
+            // To uppercase hex string.
+            Console.WriteLine(val.ToString("x4"));
+
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
