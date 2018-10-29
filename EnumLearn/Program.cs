7deea3f16d9f174e1df21111502e6c803f804f64
Adrians Heidens <adrians.heidens@gmail.com>
2018-10-29 22:08:01 +0200

    Enum examples.

---
+++ EnumLearn/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnumLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get enum from string.
+            string recordType = "NS";
+
+            // Return RecordType of throw System.ArgumentException.
+            var value = Enum.Parse<RecordType>(recordType);
+            Console.WriteLine(value);
+
+            // Try to get enum from string.
+            recordType = "A";
+            RecordType value2;
+            if (!Enum.TryParse(recordType, out value2))
+            {
+                Console.WriteLine($"RecordType '{recordType}' doesn't exist.");
+            }
+            else
+            {
+                Console.WriteLine(value2);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EnumLearn/RecordType.cs
@@ -0,0 +1,12 @@
+﻿namespace EnumLearn
+{
+    public enum RecordType : ushort
+    {
+        A = 1,
+        NS = 2,
+        MD = 3,
+        MF = 4,
+        CNAME = 5,
+        SOA = 6,
+    }
+}
