4ff0db5d3fded5fb896e0125a4a5e1106ca06c29
Adrians Heidens <adrians.heidens@gmail.com>
2018-11-02 09:41:33 +0200

    Reflection example.

---
+++ ReflectionLearn/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionLearn
+{
+    /// <summary>
+    /// This is an example program demonstrating some reflection features.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Console.WriteLine(assembly);
+
+            foreach (var type in assembly.DefinedTypes)
+            {
+                Console.WriteLine($"  Type: {type}");
+            }
+
+            foreach (var module in assembly.GetModules()) {
+                Console.WriteLine($"  Module: {module}");
+            }
+
+            var userType = assembly.GetType("ReflectionLearn.User");
+            Console.WriteLine("User Type: " + userType);
+
+            foreach (var constructor in userType.GetConstructors())
+            {
+                Console.WriteLine($"  Constructor: {constructor}");
+            }
+
+            foreach (var member in userType.GetMembers())
+            {
+                Console.WriteLine($"  Member: {member}");
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionLearn/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ReflectionLearn
+{
+    /// <summary>
+    /// An example of class for user data.
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// User name in the system.
+        /// </summary>
+        public string Name { get; set; }
+
+        public int Level { get; set; } = 0;
+
+        public User(string name)
+        {
+            Name = name;
+        }
+    }
+}
