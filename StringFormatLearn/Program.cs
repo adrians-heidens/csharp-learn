f65468c6df27e6bcb5cafab661ab44b4a412f1c0
Adrians Heidens <adrians.heidens@gmail.com>
2018-11-15 09:28:30 +0200

    String format example.

---
+++ StringFormatLearn/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringFormatLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String interpolation.
+            var foo = 12;
+            Console.WriteLine($"Variable foo={foo}.");
+
+            // String formatting with placeholders.
+            var bar = "bar";
+            string template = "Variable {0} in a spearate {0}, {1} template";
+            Console.WriteLine(string.Format(template, foo, bar));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
