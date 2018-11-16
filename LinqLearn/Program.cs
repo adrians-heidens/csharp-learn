213cd8fb4b73565c3632dfaad3730953b590141f
Adrians Heidens <adrians.heidens@gmail.com>
2018-11-16 09:40:23 +0200

    Linq example.

---
+++ LinqLearn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A linq query.
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            var numQuery = from num in numbers where (num % 2) == 0 select num;
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+            Console.WriteLine();
+
+            // Counting elements.
+            Console.WriteLine(numQuery.Count());
+
+            // Convert to array or list.
+            var numList = numQuery.ToList();
+            Console.WriteLine(string.Join(",", numList));
+
+            Console.WriteLine("Press key...");
+            Console.ReadKey();
+        }
+    }
+}
