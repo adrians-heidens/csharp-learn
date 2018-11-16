425f67af11a8efcc27f4c047eabee6f8e1a4a4d5
Adrians Heidens <adrians.heidens@gmail.com>
2018-11-16 17:12:54 +0200

    More linq examples.

--- LinqLearn/Program.cs
+++ LinqLearn/Program.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LinqLearn
 {
+    class User
+    {
+        public User(string username)
+        {
+            Username = username;
+        }
+
+        public User()
+        {
+        }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return $"User(Username='{Username}', Email='{Email}')";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -22,6 +46,46 @@ namespace LinqLearn
             // Convert to array or list.
             var numList = numQuery.ToList();
             Console.WriteLine(string.Join(",", numList));
+
+            // Filtering collection of users.
+            var userCollection = new List<User> {
+                new User { Username = "foo", Email = "foo@test", City = "Riga" },
+                new User { Username = "bar", Email = "bar@example", City = "Oslo" },
+                new User { Username = "spam", Email = "spam@test", City = "Riga" },
+                new User { Username = "eggs", Email = "eggs@test", City = "Stockholm" },
+            };
+            var q = from user in userCollection where user.Username.Length == 4 select user;
+            foreach (var user in q)
+            {
+                Console.WriteLine(user);
+            }
+
+            // Features of LINQ:
+            // Data source.
+            // Filtering.
+            // Ordering.
+            // Grouping.
+            // Joining.
+            // Selecting (Projections).
+
+            // Grouping users. This returns list of lists.
+            var userGoupQuery = from user in userCollection group user by user.City;
+            foreach (var userGroup in userGoupQuery)
+            {
+                Console.WriteLine(userGroup.Key);
+                foreach (var user in userGroup)
+                {
+                    Console.WriteLine("  {0}", user);
+                }
+            }
+
+            var usernameList = new List<string> { "foo", "bar", "eggs" };
+
+            var newUsers = from name in usernameList select new User { Username = name };
+            foreach (var user in newUsers)
+            {
+                Console.WriteLine(user);
+            }
 
             Console.WriteLine("Press key...");
             Console.ReadKey();
