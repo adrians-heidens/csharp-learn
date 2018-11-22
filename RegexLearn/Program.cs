855ba5e7e6ead02f19550fea80e9a1850e724088
Adrians Heidens <adrians.heidens@gmail.com>
2018-11-22 16:09:16 +0200

    Regex example matching/searching in multiline string.

--- RegexLearn/Program.cs
+++ RegexLearn/Program.cs
@@ -11,6 +11,20 @@ namespace RegexLearn
             Console.WriteLine(regex.IsMatch("abe"));
             Console.WriteLine(regex.IsMatch("abe-"));
             Console.WriteLine(regex.IsMatch("abe0"));
+
+            // Example of matching in multiline text and capturing a group.
+            var text = @"
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Mauris tincidunt viverra nisi, fringilla <b>posuere</b> sapien elementum at.
+Donec scelerisque <b>dictum</b> diam vitae placerat. Etiam euismod sapien
+tempor quam pulvinar,
+".Trim();
+
+            regex = new Regex(@"<b>([^<]+)</b>");
+            var match = regex.Match(text);
+            Console.WriteLine(match.Success);
+            Console.WriteLine(match.Groups[1]);
+
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
