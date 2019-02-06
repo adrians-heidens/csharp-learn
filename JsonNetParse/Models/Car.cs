aad7eb3b51a671c08104dc611148efc1fba9eb55
Adrians Heidens <adrians.heidens@gmail.com>
2019-02-06 10:14:59 +0200

    Example of modifying json on serialization.

---
+++ JsonNetParse/Models/Car.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace JsonNetParse.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public string  Manufacturer { get; set; }
+
+        // We need this for deserialization but not when serialize.
+        public IDictionary<string, string> Details { get; set; } =
+            new Dictionary<string, string>();
+
+        // We could do some tricks here or leave it all to be done with later
+        // JObject manipulations.
+        public string Layout =>
+            Details.TryGetValue("Layout", out string value) ? value : null;
+
+        public string Class =>
+            Details.TryGetValue("Class", out string value) ? value : null;
+
+        public override string ToString()
+        {
+            return "Car(" +
+                $"Model={Model}, " +
+                $"Manufacturer={Manufacturer}" +
+                ")";
+        }
+    }
+}
---
+++ JsonNetParse/TranslationExample.cs
@@ -0,0 +1,53 @@
+﻿using JsonNetParse.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace JsonNetParse
+{
+    /// <summary>
+    /// A common scenario where it is necessary to translate from one Json
+    /// representation to another.
+    /// </summary>
+    static class TranslationExample
+    {
+        public static void Run()
+        {
+            // Incoming Json.
+            var json = @"
+{
+    ""Model"": ""Focus"",
+    ""Manufacturer"": ""Ford"",
+    ""Details"": {
+        ""Class"": ""Compact"",
+        ""Layout"": ""FF""
+    }
+}
+";
+            // We aim to translate it to following.
+            /*
+{
+    ""Model"": ""Focus"",
+    ""Manufacturer"": ""Ford"",
+    ""Class"": ""Compact"",
+    ""Layout"": ""FF"",
+    ""ProcessedDate"": ""2001-02-03T00:00:00""
+}
+             */
+
+            // 1. Deserialize to expected object.
+            var car1 = JsonConvert.DeserializeObject<Car>(json);
+            Console.WriteLine(car1);
+
+            // 2. Get JObject to manipulate.
+            var carObj = JObject.FromObject(car1);
+            carObj.Remove("Details");
+            carObj.Add("ProcessedDate", new DateTime(2001, 2, 3));
+            
+            // 3. Convert JObject to Json.
+            json = JsonConvert.SerializeObject(carObj, Formatting.Indented);
+            Console.WriteLine();
+            Console.WriteLine(json);
+        }
+    }
+}
