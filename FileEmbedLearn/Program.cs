13e99853f47fb61d5baaee9632ca734566e1c024
Adrians Heidens <adrians.heidens@gmail.com>
2019-02-27 17:08:28 +0200

    Resource example.

---
+++ FileEmbedLearn/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileEmbedLearn.Properties;
+using System;
+
+namespace FileEmbedLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lipsum = Resources.lipsum;
+            Console.WriteLine(lipsum);
+
+            Console.WriteLine("Press key...");
+            Console.ReadKey();
+        }
+    }
+}
