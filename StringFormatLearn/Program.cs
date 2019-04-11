e4b2fa0c6cdf988c50294fd9bb6b787d8dbb9b61
Adrians Heidens <adrians.heidens@gmail.com>
2019-04-11 15:33:12 +0300

    Number string formatting.

--- StringFormatLearn/Program.cs
+++ StringFormatLearn/Program.cs
@@ -15,6 +15,10 @@ namespace StringFormatLearn
             string template = "Variable {0} in a spearate {0}, {1} template";
             Console.WriteLine(string.Format(template, foo, bar));
 
+            // Formatting numbers.
+            Console.WriteLine($"Number padded: {foo.ToString("D4")}");
+            Console.WriteLine($"Number padded: {foo:D6}");
+
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
