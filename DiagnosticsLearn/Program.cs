4efab6b6b5935c30548f6e3d06330c57c3c64efc
Adrians Heidens <adrians.heidens@gmail.com>
2019-11-12 15:19:16 +0200

    Diagnostics trace listener example.

---
+++ DiagnosticsLearn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DiagnosticsLearn
+{
+    public class SomeTraceListener : TraceListener
+    {
+        private readonly string _prefix;
+        
+        public SomeTraceListener(string prefix)
+        {
+            _prefix = prefix;
+        }
+        
+        public override void Write(string message)
+        {
+            Console.Write(_prefix + message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine(_prefix + message);
+        }
+    }
+    
+    /// <summary>
+    /// Demonstrates Trace listener usage.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new SomeTraceListener(">>> "));
+            Trace.WriteLine("Hello trace message!");
+            
+        }
+    }
+}
